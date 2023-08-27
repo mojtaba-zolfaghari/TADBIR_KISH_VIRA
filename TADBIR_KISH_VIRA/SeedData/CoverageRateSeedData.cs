779e2a9fc8dd84061673d0db705d4e70acb54fb5
m.zolfaghari <mzolfaghari71@yahoo.com>
2023-08-27 14:52:36 +0330

    lastest changes

--- TADBIR_KISH_VIRA/SeedData/CoverageRateSeedData.cs
+++ TADBIR_KISH_VIRA/SeedData/CoverageRateSeedData.cs
@@ -1,6 +1,25 @@
-﻿namespace TADBIR_KISH_VIRA.SeedData
+﻿using TADBIR_KISH_VIRA.Data;
+using TADBIR_KISH_VIRA.Models;
+
+namespace TADBIR_KISH_VIRA.SeedData
 {
     public class CoverageRateSeedData
     {
+        public static void Seed(ApplicationDbContext context)
+        {
+            if (!context.CoverageRates.Any())
+            {
+                var coverageRates = new List<CoverageRate>
+            {
+                new CoverageRate { CoverageType = 1, Rate = 0.0052m },
+                new CoverageRate { CoverageType = 2, Rate = 0.0042m },
+                new CoverageRate { CoverageType = 3, Rate = 0.005m }
+            };
+
+                context.CoverageRates.AddRange(coverageRates);
+                context.SaveChanges();
+            }
+        }
     }
+
 }
